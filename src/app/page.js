cfe71d63dba26dcd9cee6866ee4ea0518346800a
RikeshThapa <rthapa@u.rochester.edu>
2024-02-22 15:44:09 -0800

    adding Microsoft clarity to track

--- src/app/page.js
+++ src/app/page.js
@@ -12,17 +12,26 @@ import FAQ from '@/components/faq'
 export default function Home() {
 
   return (
-    <StateProvider>
-      <div className="p-10 flex flex-col place-items-center">
-        <QuickNav />
-        <div className='text-center max-w-screen overflow-hidden'>
-          <LandingPage />
-        </div>
-        <div className="">
-            <ActionMenu />
-        </div>
-          <FAQ />
-      </div>
-    </StateProvider>
+    <>
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "l5zqf94lap");
+        </script>
+        <StateProvider>
+          <div className="p-10 flex flex-col place-items-center">
+            <QuickNav />
+            <div className='text-center max-w-screen overflow-hidden'>
+              <LandingPage />
+            </div>
+            <div className="">
+                <ActionMenu />
+            </div>
+              <FAQ />
+          </div>
+        </StateProvider>
+    </>
   )
 }
