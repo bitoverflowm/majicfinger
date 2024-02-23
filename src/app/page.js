dec18a9ab88cd6a59d2d0e45b9ea527dff5d30db
RikeshThapa <rthapa@u.rochester.edu>
2024-02-22 16:53:17 -0800

    adding fix for clarity import

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 //import React, { useState, useEffect } from 'react'
 
 //import { CsvToHtmlTable } from 'react-csv-to-table';
+import Script from 'next/script'
 import { StateProvider } from '@/context/stateContext'
 
 import ActionMenu from '@/components/actionMenu'
@@ -13,13 +14,10 @@ export default function Home() {
 
   return (
     <>
-        <script type="text/javascript">
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "l5zqf94lap");
-        </script>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.clarity.ms/tag/l5zqf94lap"
+        />
         <StateProvider>
           <div className="p-10 flex flex-col place-items-center">
             <QuickNav />
