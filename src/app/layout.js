5f039906941d545e85878a34421d55e000d841da
RikeshThapa <rthapa@u.rochester.edu>
2024-03-04 22:23:15 -0800

    adding meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,14 @@
 import Nav from '@/components/nav';
-
+import { Metadata } from 'next';
 import './globals.css'
+ 
+export const metadata = {
+  title: 'Lychee',
+  description: 'Analyze and Visualize Data. Without Spreadsheets.',
+  custom: [
+    { name: '_foundr', content: '1e04ca29b3242af44ee799e756cfafce' },
+  ]
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -9,7 +17,7 @@ export default function RootLayout({ children }) {
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      
       <body>
         <Nav/>
         <div className='-mt-16'>
