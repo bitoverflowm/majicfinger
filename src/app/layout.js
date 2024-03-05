5c775e26b6d5ea360eb47da3147b31b842af0532
RikeshThapa <rthapa@u.rochester.edu>
2024-03-04 22:41:08 -0800

    adding verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,12 @@
 import Nav from '@/components/nav';
-import { Metadata } from 'next';
 import './globals.css'
  
 export const metadata = {
   title: 'Lychee',
   description: 'Analyze and Visualize Data. Without Spreadsheets.',
-  custom: [
-    { name: '_foundr', content: '1e04ca29b3242af44ee799e756cfafce' },
-  ]
+  verification: {
+    other: { _foundr: ['1e04ca29b3242af44ee799e756cfafce'] },
+  }
 };
 
 export default function RootLayout({ children }) {
