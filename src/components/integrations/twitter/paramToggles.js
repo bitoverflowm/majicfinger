b2578366cf2baa3f730c096a9f3deaf1fba5201b
RikeshThapa <rthapa@u.rochester.edu>
2024-05-14 22:54:03 -0700

    twitter api access is working, and I think I like how its all looking and coming together

--- src/components/integrations/twitter/paramToggles.js
+++ src/components/integrations/twitter/paramToggles.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Badge } from '@/components/ui/badge';
 
 const ParamToggles = ({field_type, val, toggle, arr}) => {
     // Your component logic here
 
     return (
-        <div className={`${arr.includes(val) ? 'bg-lychee-green text-white hover:bg-white hover:text-black': 'bg-white text-black hover:bg-lychee-green hover:text-white'} text-xs border-slate-200 border px-2 p-1 rounded-md cursor-pointer m-1 my-2 `} onClick={()=>toggle(field_type,  val)}>{val}</div>
+        <Badge variant={`${arr.includes(val) ? 'default': 'outline'}`} className={`${arr.includes(val) ? 'bg-black text-white hover:bg-slate-500 hover:text-black': 'hover:bg-slate-300 hover:text-black'} cursor-pointer m-1`} onClick={()=>toggle(field_type,  val)}>{val}</Badge>
     );
 };
 
--- src/components/integrationsView/integrationPlayground/index.js
+++ src/components/integrationsView/integrationPlayground/index.js
@@ -17,18 +17,40 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+
 import { Textarea } from "@/components/ui/textarea"
 
+import { useMyStateV2  } from '@/context/stateContextV2'
+
+
+//static data
 import { params } from '@/components/integrations/twitter/params';
+import twitterDemoData from '@/components/integrations/twitter/twitterDemoData';
+
 import ParamToggles from "@/components/integrations/twitter/paramToggles"
+import PreviewGrid from "@/components/gridView/previewGrid"
+
 
   
 const IntegrationPlayground = () => {
+    const contextStateV2 = useMyStateV2()
+
+    const connectedData = contextStateV2?.connectedData || [];
+    const setConnectedData = contextStateV2?.setConnectedData || [];
+    /* Step Names:  */
     const [stepName, setStepName] = useState('')
     const [expansions, setExpansions] = useState([]);
     const [userFields, setUserFields] = useState([]);
     const [tweetFields, setTweetFields] = useState([]);
     const [listFields, setListFields] = useState([]);
+
+    const [loading, setLoading] = useState()
+    const [prevData, setPrevData] = useState()
+    const [userHandleIds, setUserHandleIds] = useState([{}])
+    const [rateInfo, setRateInfo] = useState()
+    const [searchingUserName, setSearchingUserName] = useState()
+
+    const [demoUserNames] = useState(['elonmusk', 'justinbieber', 'christiano', 'jack', 'lychee_xyz', 'spaceX'])// 'kanyewest', 'BarackObama', 'lychee_xyz', 'spaceX'])
 
     const toggleParams = (fieldType, value) => {
         if(fieldType === 'expansions'){
@@ -55,6 +77,96 @@ const IntegrationPlayground = () => {
             } else {
                 setListFields([...listFields, value]);
             }
+        }
+    }
+
+    const fetchTwitterHandler = async (ask) => {
+        setLoading(true);
+
+        try {
+            // Append the handle as a query parameter in the URL
+            if(ask === 'user_by_handle' ){
+                if(demoUserNames.includes(searchingUserName)){
+                    let newHandle = {
+                        searchingUserName: {
+                            id: twitterDemoData[searchingUserName].userData.id
+                        }
+                    }
+                    setUserHandleIds(prevUserHandles => [...prevUserHandles, newHandle]);
+                    console.log(twitterDemoData[searchingUserName].userData)
+                    setConnectedData([twitterDemoData[searchingUserName].userData])
+                }else{
+                    const url = new URL('/api/integrations/twitter/userhandle', window.location.origin);
+                    url.searchParams.append('handle', searchingUserName);
+                    let allExpansions = params['user_by_handle'].expansions
+                    let allUserFields = params['user_by_handle'].userFields
+                    let allTweetFields = params['user_by_handle'].tweetFields
+                    let res = await fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({ expansions: allExpansions, userFields: allUserFields, tweetFields: allTweetFields}),
+                    });
+
+                    if (res.status === 200) {
+                        let resUserData = await res.json();
+                        console.log(resUserData);
+                        setConnectedData([resUserData.userData]);
+                        //setStagingData(resUserData)
+                        let newHandle = {
+                            userHandle: {
+                                id: resUserData.userData.id
+                            }
+                        }
+                        setUserHandleIds(prevUserHandles => [...prevUserHandles, newHandle]);
+                        setRateInfo(resUserData.rateLimit)
+                    } else {
+                        console.error("Twitter User Data pull failed");
+                    }
+                }
+            }else{
+                if(demoUserNames.includes(searchingUserName)){
+                    //let thirdParam = tweetFields.length > 0 ? tweetFields : listFields
+                    if(ask === 'user_likes_by_id'){
+                        setConnectedData(twitterDemoData[searchingUserName].liked_tweets)                     
+                    }else if(ask === 'user_owned_lists_by_id'){
+                        setConnectedData(twitterDemoData[searchingUserName].owned_lists)
+                    } else if(ask === 'user_pinned_tweet'){
+                        console.log("user pimnmed tweet", twitterDemoData[searchingUserName].userData)
+                        setConnectedData([twitterDemoData[searchingUserName].userData])
+                    }
+                }else{
+                    const url = new URL(`/api/integrations/twitter/${ask}`, window.location.origin);
+                    url.searchParams.append('handleId', userHandleIds[searchingUserName]);
+                    let thirdParam = tweetFields.length > 0 ? tweetFields : listFields
+                    let res = await fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({ expansions, userFields, thirdParam }),
+                    });
+
+                    if (res.status === 200) {
+                        let resData = await res.json();
+                        console.log(resData);
+                        //setPrevData(resData.userData);
+                        setConnectedData(resData.userData)
+                        setRateInfo(resData.rateLimit)
+                    } else {
+                        console.error("Twitter User Data pull failed");
+                    }
+                }                
+            }
+        } catch (error) {
+            console.error("Fetch error:", error);
+        } finally {
+            setLoading(false);
+            setExpansions([])
+            setUserFields([])
+            setTweetFields([])
+            setListFields([])
         }
     }
 
@@ -103,6 +215,22 @@ const IntegrationPlayground = () => {
                         <SelectValue placeholder="Pick an option to search for?" />
                       </SelectTrigger>
                       <SelectContent>
+                        <SelectItem value="user_by_handle">
+                          <div className="flex items-start gap-3 text-muted-foreground">
+                            <Rabbit className="size-5" />
+                            <div className="grid gap-0.5">
+                              <p>
+                                <span className="font-medium text-foreground">
+                                  User
+                                </span>
+                                {" "}by Handle
+                              </p>
+                              <p className="text-xs" data-description>
+                                -
+                              </p>
+                            </div>
+                          </div>
+                        </SelectItem>
                         <SelectItem value="user_pinned_tweet">
                           <div className="flex items-start gap-3 text-muted-foreground">
                             <Rabbit className="size-5" />
@@ -111,7 +239,7 @@ const IntegrationPlayground = () => {
                                 <span className="font-medium text-foreground">
                                   User's
                                 </span>
-                                {" "}Pinned Tweet
+                                {" "}Pinned Tweet by handle
                               </p>
                               <p className="text-xs" data-description>
                                 -
@@ -127,7 +255,7 @@ const IntegrationPlayground = () => {
                                 <span className="font-medium text-foreground">
                                   User's
                                 </span>
-                                {" "} Lists
+                                {" "} Lists by handle
                               </p>
                               <p className="text-xs" data-description>
                                 -
@@ -138,57 +266,64 @@ const IntegrationPlayground = () => {
                       </SelectContent>
                     </Select>
                   </div>
-                  <div className='pt-2'>
+                  {
+                    stepName === 'user_by_handle'
+                     && <div className="grid gap-3">
+                            <Label htmlFor="userhandle">User Handle</Label>
+                            <Input id="userHandle" type="text" placeholder="enter userhandle without @" onChange={(e)=>setSearchingUserName(e.target.value)} />
+                        </div>
+                  }
+                  <div className=''>
                     <div className='text-sm text-muted-foreground'>Select the granular data points you want to examine</div>
                     <div className='text-sm text-muted-foreground'>These will be your "columns"</div>
-                </div>
-                {
-                    stepName && params[stepName].expansions &&
-                        <>
-                            <div className='font-bold text-slate-600 text-xs'></div>
-                            <div className='flex flex-wrap pinned_tweet_tour'>
-                                {params[stepName].expansions.map((val) => (
-                                    <ParamToggles key={val} field_type="expansions" val={val} toggle={() => toggleParams('expansions', val)} arr={expansions}/>
-                                ))}
+                  </div>
+                    {
+                        stepName && params[stepName].expansions &&
+                            <>
+                                <div className='font-bold text-slate-600 text-xs'></div>
+                                <div className='flex flex-wrap pinned_tweet_tour'>
+                                    {params[stepName].expansions.map((val) => (
+                                        <ParamToggles key={val} field_type="expansions" val={val} toggle={() => toggleParams('expansions', val)} arr={expansions}/>
+                                    ))}
+                                </div>
+                            </>
+                    }
+                    {
+                        stepName && params[stepName].tweetFields &&
+                            <div className=''>
+                                <div className='font-bold text-slate-600 text-xs'>Tweet Details</div>
+                                {/* For tweetFields */}
+                                <div className='flex flex-wrap'>
+                                    {params[stepName].tweetFields.map((val) => (
+                                        <ParamToggles key={val} field_type="tweetFields" val={val} toggle={() => toggleParams('tweetFields', val)} arr={tweetFields}/>
+                                    ))}
+                                </div>
                             </div>
-                        </>
-                }
-                {
-                    stepName && params[stepName].tweetFields &&
-                        <div className=''>
-                            <div className='font-bold text-slate-600 text-xs'>Tweet Details</div>
-                            {/* For tweetFields */}
-                            <div className='flex flex-wrap'>
-                                {params[stepName].tweetFields.map((val) => (
-                                    <ParamToggles key={val} field_type="tweetFields" val={val} toggle={() => toggleParams('tweetFields', val)} arr={tweetFields}/>
-                                ))}
+                    }
+                    {
+                        stepName && params[stepName].userFields &&
+                            <div className=''>
+                                <div className='font-bold text-slate-600 text-xs'>User Details</div>
+                                {/* For tweetFields */}
+                                <div className='flex flex-wrap'>
+                                    {params[stepName].userFields.map((val) => (
+                                        <ParamToggles key={val} field_type="userFields" val={val} toggle={() => toggleParams('userFields', val)} arr={userFields}/>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                }
-                {
-                    stepName && params[stepName].userFields &&
-                        <div className=''>
-                            <div className='font-bold text-slate-600 text-xs'>User Details</div>
-                            {/* For tweetFields */}
-                            <div className='flex flex-wrap'>
-                                {params[stepName].userFields.map((val) => (
-                                    <ParamToggles key={val} field_type="userFields" val={val} toggle={() => toggleParams('userFields', val)} arr={userFields}/>
-                                ))}
+                    }
+                    {
+                        stepName && params[stepName]['list.fields'] &&
+                            <div className=''>
+                                <div className='font-bold text-slate-600 text-xs'>List Details</div>
+                                {/* For tweetFields */}
+                                <div className='flex flex-wrap'>
+                                    {params[stepName]['list.fields'].map((val) => (
+                                        <ParamToggles key={val} field_type="listFields" val={val} toggle={() => toggleParams('listFields', val)} arr={listFields}/>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                }
-                {
-                    stepName && params[stepName]['list.fields'] &&
-                        <div className=''>
-                            <div className='font-bold text-slate-600 text-xs'>List Details</div>
-                            {/* For tweetFields */}
-                            <div className='flex flex-wrap'>
-                                {params[stepName]['list.fields'].map((val) => (
-                                    <ParamToggles key={val} field_type="listFields" val={val} toggle={() => toggleParams('listFields', val)} arr={listFields}/>
-                                ))}
-                            </div>
-                        </div>
-                }
+                    }
                     <div className='flex place-content-end gap-4 text-xs'>
                         <div className='px-3 py-1 bg-white text-lychee-red border border-lychee-red cursor-pointer hover:bg-lychee-red hover:text-white rounded-md' onClick={()=>clearHandler()}>Clear</div>
                         <div className='shadow-sm px-3 py-1 bg-lychee-go text-lychee-black border border-lychee-go cursor-pointer hover:bg-lychee-green hover:text-white hover:border-lychee-green rounded-md hover:shadow-lychee-green hover:shadow-2xl' onClick={()=>fetchTwitterHandler(stepName)}>Connect</div>
@@ -200,7 +335,11 @@ const IntegrationPlayground = () => {
               <Badge variant="outline" className="absolute right-3 top-3">
                 Output
               </Badge>
-              <div className="flex-1" />
+              <div className="flex-1">
+                {
+                     connectedData && <PreviewGrid />
+                }
+              </div>
             </div>
           </main>
         </div>
