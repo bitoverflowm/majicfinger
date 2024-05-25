722a1a2dc8b31ab5a45044b18fec1cc9036086d8
RikeshThapa <rthapa@u.rochester.edu>
2024-05-24 18:49:05 -0700

    deleting columns is now possible

--- src/components/gridView/menu/index.js
+++ src/components/gridView/menu/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import {
     Menubar,
     MenubarCheckboxItem,
@@ -14,176 +16,147 @@ import {
     MenubarSubTrigger,
     MenubarTrigger,
   } from "@/components/ui/menubar"
+
+  import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer"
+  
+  import { toast } from "sonner"
+
+  import { Button } from "@/components/ui/button"
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import { Trash } from "lucide-react"
+
+  import { useMyStateV2  } from '@/context/stateContextV2'
+
   
   export function Menu() {
+    const contextStateV2 = useMyStateV2()
+
+    let connectedCols = contextStateV2?.connectedCols || []
+    let setConnectedCols = contextStateV2?.setConnectedCols || []
+    let connectedData = contextStateV2?.connectedData || []
+    let setConnectedData = contextStateV2?.setConnectedData || []
+
+    const [open, setOpen] = useState()
+    const [content, setContent] = useState()
+    
+
+    const handleOpen = (param) => {
+      setContent(param)
+      if(param ==="editColumns"){
+        setOpen(true)
+      }
+    }
+
+    const handleAddColumn = (name) => {
+      let newCols = [...connectedCols, { field: name }];
+      setConnectedCols(newCols);
+      //adding col to data as well
+      if(connectedData.length > 0){
+          setConnectedData(prevData => {
+              // Create a new array with updated data
+              const newData = prevData.map((item, index) => {
+                  return { ...item, [name]: '' }; // Update the specific field value
+              });
+              return newData;
+            });
+      }else{
+          setConnectedData(prevData => {
+              return prevData.map(item => {
+                return { ...item, [name]: '' };
+              }).concat([{ [name]: '' }]);
+            });
+      }
+
+      toast(`New Column added!`, {
+          duration: 5000
+      })   
+    }
+
+    const handleDeleteColumn = (index) => {
+      // Get the name of the column to be deleted
+      const colName = connectedCols[index].field;
+      
+      // Remove the column from the connectedCols array
+      let newCols = connectedCols.filter((_, colIndex) => colIndex !== index);
+      setConnectedCols(newCols);
+    
+      // Remove the column from each item in the connectedData array
+      setConnectedData(prevData => {
+        return prevData.map(item => {
+          const newItem = { ...item };
+          delete newItem[colName]; // Remove the specific field
+          return newItem;
+        });
+      });
+    
+    
+      toast(`Column "${colName}" deleted!`, {
+        duration: 5000
+      });
+    };
+
     return (
       <Menubar className="rounded-xl border-slate-100 border px-2 lg:px-4">
         <MenubarMenu>
           <MenubarTrigger>Columns</MenubarTrigger>
           <MenubarContent>
-            <MenubarItem>Edit Columns</MenubarItem>
+            <MenubarItem onClick={()=>handleOpen('editColumns')}>
+                Edit Columns
+            </MenubarItem>
             <MenubarItem>Delete Columns</MenubarItem>
             <MenubarSeparator />
             <MenubarItem>Rearrange Column Sequence</MenubarItem>
             <MenubarItem>Hide Columns</MenubarItem>
           </MenubarContent>
         </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger className="relative">File</MenubarTrigger>
-          <MenubarContent>
-            <MenubarSub>
-              <MenubarSubTrigger>New</MenubarSubTrigger>
-              <MenubarSubContent className="w-[230px]">
-                <MenubarItem>
-                  Playlist <MenubarShortcut>⌘N</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem disabled>
-                  Playlist from Selection <MenubarShortcut>⇧⌘N</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem>
-                  Smart Playlist... <MenubarShortcut>⌥⌘N</MenubarShortcut>
-                </MenubarItem>
-                <MenubarItem>Playlist Folder</MenubarItem>
-                <MenubarItem disabled>Genius Playlist</MenubarItem>
-              </MenubarSubContent>
-            </MenubarSub>
-            <MenubarItem>
-              Open Stream URL... <MenubarShortcut>⌘U</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem>
-              Close Window <MenubarShortcut>⌘W</MenubarShortcut>
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarSub>
-              <MenubarSubTrigger>Library</MenubarSubTrigger>
-              <MenubarSubContent>
-                <MenubarItem>Update Cloud Library</MenubarItem>
-                <MenubarItem>Update Genius</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Organize Library...</MenubarItem>
-                <MenubarItem>Export Library...</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Import Playlist...</MenubarItem>
-                <MenubarItem disabled>Export Playlist...</MenubarItem>
-                <MenubarItem>Show Duplicate Items</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Get Album Artwork</MenubarItem>
-                <MenubarItem disabled>Get Track Names</MenubarItem>
-              </MenubarSubContent>
-            </MenubarSub>
-            <MenubarItem>
-              Import... <MenubarShortcut>⌘O</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem disabled>Burn Playlist to Disc...</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>
-              Show in Finder <MenubarShortcut>⇧⌘R</MenubarShortcut>{" "}
-            </MenubarItem>
-            <MenubarItem>Convert</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Page Setup...</MenubarItem>
-            <MenubarItem disabled>
-              Print... <MenubarShortcut>⌘P</MenubarShortcut>
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Edit</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem disabled>
-              Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem disabled>
-              Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem disabled>
-              Cut <MenubarShortcut>⌘X</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem disabled>
-              Copy <MenubarShortcut>⌘C</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem disabled>
-              Paste <MenubarShortcut>⌘V</MenubarShortcut>
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>
-              Select All <MenubarShortcut>⌘A</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem disabled>
-              Deselect All <MenubarShortcut>⇧⌘A</MenubarShortcut>
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>
-              Smart Dictation...{" "}
-              <MenubarShortcut>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="m12 8-9.04 9.06a2.82 2.82 0 1 0 3.98 3.98L16 12" />
-                  <circle cx="17" cy="7" r="5" />
-                </svg>
-              </MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem>
-              Emoji & Symbols{" "}
-              <MenubarShortcut>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4"
-                  viewBox="0 0 24 24"
-                >
-                  <circle cx="12" cy="12" r="10" />
-                  <path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
-                </svg>
-              </MenubarShortcut>
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>View</MenubarTrigger>
-          <MenubarContent>
-            <MenubarCheckboxItem>Show Playing Next</MenubarCheckboxItem>
-            <MenubarCheckboxItem checked>Show Lyrics</MenubarCheckboxItem>
-            <MenubarSeparator />
-            <MenubarItem inset disabled>
-              Show Status Bar
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem inset>Hide Sidebar</MenubarItem>
-            <MenubarItem disabled inset>
-              Enter Full Screen
-            </MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger className="hidden md:block">Account</MenubarTrigger>
-          <MenubarContent forceMount>
-            <MenubarLabel inset>Switch Account</MenubarLabel>
-            <MenubarSeparator />
-            <MenubarRadioGroup value="benoit">
-              <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
-              <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
-              <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
-            </MenubarRadioGroup>
-            <MenubarSeparator />
-            <MenubarItem inset>Manage Family...</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem inset>Add Account...</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
+        {/* Universal Drawer */}
+        <Drawer open={open} onOpenChange={setOpen}>
+          {
+            content ==="editColumns" &&
+              <DrawerContent>
+                  <div className="mx-auto w-64">              
+                      <DrawerHeader>
+                          <DrawerTitle className="text-center">Edit Columns</DrawerTitle>
+                      </DrawerHeader>
+                      {connectedCols.map((col, index) => (
+                        <div key={index} className="grid gap-4">
+                          <div className="grid gap-2">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                              <Label htmlFor={`col-${index}`}>{col.field}</Label>
+                              <Input
+                                id={`col-${index}`}
+                                defaultValue={col.field}
+                                className="col-span-2 h-8"
+                              />
+                              <div
+                                className="bg-red-600 w-6 h-6 rounded-sm flex place-items-center place-content-center text-white cursor-pointer hover:bg-white hover:text-red-600 border-2 border-red-600"
+                                onClick={() => handleDeleteColumn(index)}
+                              >
+                                <Trash className="w-3 h-3" />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                  </div>
+                  <DrawerFooter className={'mx-auto'}>
+                      <DrawerClose asChild className="w-32">
+                          <Button variant="outline">Close</Button>
+                      </DrawerClose>
+                  </DrawerFooter>
+              </DrawerContent>
+          }
+        </Drawer>
       </Menubar>
     )
   }
