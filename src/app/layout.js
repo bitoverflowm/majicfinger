46aebf182745891e64390bf9072b71b4ab4753ba
RikeshThapa <rthapa@u.rochester.edu>
2024-07-15 22:46:05 -0700

    adding layouts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,37 @@
-//import Nav from '@/components/nav';
+// import Nav from '@/components/nav';
 import './globals.css'
 import { Toaster } from "@/components/ui/toaster"
 
+// import { StateProvider } from '@/context/stateContext'
 
-//import { StateProvider } from '@/context/stateContext'
- 
 export const metadata = {
-  title: 'Lychee',
-  description: 'Find, get, analyze and visualize Data supercharged with AI. The best alternative to Excel and Google Sheets.',
+  title: 'Easy Charts - Refreshingly simple charts.',
+  description: 'Create stunning charts effortlessly with Easy Charts. No subscriptions, unlimited exports, and fully customizable. Get started today and simplify your data visualization.',
   verification: {
     other: { _foundr: ['973f2a49e12fad2ad799756c823a6d3b'] },
+  },
+  openGraph: {
+    url: 'https://www.lych3e.com/charts',
+    type: 'website',
+    title: 'Easy Charts - Refreshingly simple charts. 0Code shadcn/charts.',
+    description: 'Create stunning charts effortlessly with Easy Charts. No Code Shdcn Charts. No subscriptions, unlimited exports, and fully customizable. Get started today and simplify your data visualization.',
+    siteName: 'Easy Charts',
+    images: [
+      {
+        url: 'https://www.lych3e.com/ogImage.png',
+        width: 1200,
+        height: 630,
+        alt: 'Easy Charts OG Image'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    domain: 'lych3e.com/charts',
+    url: 'https://www.lych3e.com/charts',
+    title: 'Easy Charts - Refreshingly simple charts. 0Code shadcn/charts.',
+    description: 'Create stunning charts effortlessly with Easy Charts. No Code Shdcn Charts. No subscriptions, unlimited exports, and fully customizable. Get started today and simplify your data visualization.',
+    image: 'https://www.lych3e.com/ogImage.png'
   }
 };
 
