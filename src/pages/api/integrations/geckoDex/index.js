fa4271ea4f7d9550e6a3c8b75ec7aa9935d8c261
RikeshThapa <rthapa@u.rochester.edu>
2024-07-22 00:46:29 -0700

    adding pools and complete

--- src/pages/api/integrations/geckoDex/index.js
+++ src/pages/api/integrations/geckoDex/index.js
@@ -1,6 +1,6 @@
 
 export default async (req, res) => {
-    const { query, search, network, addresses } = req.query
+    const { query, search, network, addresses, dex } = req.query
     let url
     switch(query){        
         case 'networks':
@@ -32,8 +32,44 @@ export default async (req, res) => {
                 return res.status(400).json({ message: 'Missing network parameter' });
             }
             url = `https://api.geckoterminal.com/api/v2/networks/${network}/dexes`;
-            console.log(url)
             return await dexes(url, res);
+        case 'networkTrendingPools':
+            if (!network) {
+                return res.status(400).json({ message: 'Missing network parameter' });
+            }
+            url = `https://api.geckoterminal.com/api/v2/networks/${network}/trending_pools`;
+            return await genericPools(url, res);
+        case 'specificPool':
+            if (!network || !addresses) {
+                return res.status(400).json({ message: 'Missing network or addresses parameter' });
+            }
+            url = `https://api.geckoterminal.com/api/v2/networks/${network}/pools/${addresses}`;
+            return await specificPool(url, res);
+        case 'multiplePools':
+            if (!network || !addresses) {
+                return res.status(400).json({ message: 'Missing network or addresses parameter' });
+            }
+            url = `https://api.geckoterminal.com/api/v2/networks/${network}/pools/multi/${addresses}`;
+            return await multiplePools(url, res);
+        case 'topPools':
+            if (!network) {
+                return res.status(400).json({ message: 'Missing network parameter' });
+            }
+            url = `https://api.geckoterminal.com/api/v2/networks/${network}/pools`;
+            return await multiplePools(url, res);
+        case 'dexTopPools':
+            if (!dex) {
+                return res.status(400).json({ message: 'Missing dex parameter' });
+            }
+            url = `https://api.geckoterminal.com/api/v2/networks/${network}/dexes/${dex}/pools`;
+            return await multiplePools(url, res);        
+        case 'newPoolsNetwork':
+            if (!network) {
+                return res.status(400).json({ message: 'Missing network parameter' });
+            }
+            url = `https://api.geckoterminal.com/api/v2/networks/${network}/new_pools`;
+            return await multiplePools(url, res);       
+        
         default:
             return res.status(400).json({ message: 'Invalid query parameter' });
     }
@@ -445,6 +481,282 @@ const dexes = async (url, res) => {
     }
 };
 
+
+//pools
+
+const genericPools = async (url, res) => {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.status === 200) {
+            const { data } = await response.json();
+
+            console.log(data)
+
+            const flattenedData = data.map((val) => {
+                const {
+                    id,
+                    type,
+                    attributes: {
+                        name,
+                        address,
+                        base_token_price_usd,
+                        quote_token_price_usd,
+                        base_token_price_native_currency,
+                        quote_token_price_native_currency,
+                        base_token_price_quote_token,
+                        quote_token_price_base_token,
+                        pool_created_at,
+                        reserve_in_usd,
+                        fdv_usd,
+                        market_cap_usd,
+                        price_change_percentage,
+                        transactions,
+                        volume_usd
+                    }
+                } = val;
+
+                return {
+                    id,
+                    type,
+                    name,
+                    address,
+                    base_token_price_usd: roundedTo4Decimals(base_token_price_usd),
+                    quote_token_price_usd: roundedTo4Decimals(quote_token_price_usd),
+                    base_token_price_native_currency: roundedTo4Decimals(base_token_price_native_currency),
+                    quote_token_price_native_currency: roundedTo4Decimals(quote_token_price_native_currency),
+                    base_token_price_quote_token: roundedTo4Decimals(base_token_price_quote_token),
+                    quote_token_price_base_token: roundedTo4Decimals(quote_token_price_base_token),
+                    pool_created_at,
+                    reserve_in_usd: roundedTo4Decimals(reserve_in_usd),
+                    fdv_usd,
+                    market_cap_usd,
+                    price_change_percentage_day: price_change_percentage ? {
+                        day: price_change_percentage.day,
+                        week: price_change_percentage.week,
+                        month: price_change_percentage.month
+                    } : {},
+                    transactions: transactions ? {
+                        count: transactions.count,
+                        volume: transactions.volume
+                    } : {},
+                    volume_usd: volume_usd ? {
+                        day: volume_usd.day,
+                        week: volume_usd.week,
+                        month: volume_usd.month
+                    } : {}
+                };
+            });
+
+            return res.status(200).json(flattenedData);
+        } else {
+            return res.status(response.status).json({ message: 'New Pools CoinGecko Data pull failed' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+const specificPool = async (url, res) => {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.status === 200) {
+            const { data } = await response.json();
+            const {
+                id,
+                type,
+                attributes: {
+                    name,
+                    address,
+                    base_token_price_usd,
+                    quote_token_price_usd,
+                    base_token_price_native_currency,
+                    quote_token_price_native_currency,
+                    base_token_price_quote_token,
+                    quote_token_price_base_token,
+                    pool_created_at,
+                    reserve_in_usd,
+                    fdv_usd,
+                    market_cap_usd,
+                    price_change_percentage,
+                    transactions,
+                    volume_usd
+                },
+                relationships: {
+                    base_token,
+                    quote_token,
+                    dex
+                }
+            } = data;
+
+            const flattenedData = {
+                id,
+                type,
+                name,
+                address,
+                base_token_price_usd: roundedTo4Decimals(base_token_price_usd),
+                quote_token_price_usd: roundedTo4Decimals(quote_token_price_usd),
+                base_token_price_native_currency: roundedTo4Decimals(base_token_price_native_currency),
+                quote_token_price_native_currency: roundedTo4Decimals(quote_token_price_native_currency),
+                base_token_price_quote_token: roundedTo4Decimals(base_token_price_quote_token),
+                quote_token_price_base_token: roundedTo4Decimals(quote_token_price_base_token),
+                pool_created_at,
+                reserve_in_usd: roundedTo4Decimals(reserve_in_usd),
+                fdv_usd,
+                market_cap_usd,
+                price_change_percentage_m5: price_change_percentage ? price_change_percentage.m5 : null,
+                price_change_percentage_h1: price_change_percentage ? price_change_percentage.h1 : null,
+                price_change_percentage_h6: price_change_percentage ? price_change_percentage.h6 : null,
+                price_change_percentage_h24: price_change_percentage ? price_change_percentage.h24 : null,
+                transactions_m5_buys: transactions ? transactions.m5.buys : null,
+                transactions_m5_sells: transactions ? transactions.m5.sells : null,
+                transactions_m5_buyers: transactions ? transactions.m5.buyers : null,
+                transactions_m5_sellers: transactions ? transactions.m5.sellers : null,
+                transactions_m15_buys: transactions ? transactions.m15.buys : null,
+                transactions_m15_sells: transactions ? transactions.m15.sells : null,
+                transactions_m15_buyers: transactions ? transactions.m15.buyers : null,
+                transactions_m15_sellers: transactions ? transactions.m15.sellers : null,
+                transactions_m30_buys: transactions ? transactions.m30.buys : null,
+                transactions_m30_sells: transactions ? transactions.m30.sells : null,
+                transactions_m30_buyers: transactions ? transactions.m30.buyers : null,
+                transactions_m30_sellers: transactions ? transactions.m30.sellers : null,
+                transactions_h1_buys: transactions ? transactions.h1.buys : null,
+                transactions_h1_sells: transactions ? transactions.h1.sells : null,
+                transactions_h1_buyers: transactions ? transactions.h1.buyers : null,
+                transactions_h1_sellers: transactions ? transactions.h1.sellers : null,
+                transactions_h24_buys: transactions ? transactions.h24.buys : null,
+                transactions_h24_sells: transactions ? transactions.h24.sells : null,
+                transactions_h24_buyers: transactions ? transactions.h24.buyers : null,
+                transactions_h24_sellers: transactions ? transactions.h24.sellers : null,
+                volume_usd_m5: volume_usd ? volume_usd.m5 : null,
+                volume_usd_h1: volume_usd ? volume_usd.h1 : null,
+                volume_usd_h6: volume_usd ? volume_usd.h6 : null,
+                volume_usd_h24: volume_usd ? volume_usd.h24 : null,
+                base_token_id: base_token ? base_token.data.id : null,
+                quote_token_id: quote_token ? quote_token.data.id : null,
+                dex_id: dex ? dex.data.id : null
+            };
+
+            return res.status(200).json([flattenedData]);
+        } else {
+            return res.status(response.status).json({ message: 'Specific Pool CoinGecko Data pull failed' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+const multiplePools = async (url, res) => {
+    try {
+        console.log(url)
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.status === 200) {
+            const { data } = await response.json();
+
+            console.log(data)
+
+            const flattenedData = data.map((pool) => {
+                const {
+                    id,
+                    type,
+                    attributes: {
+                        name,
+                        address,
+                        base_token_price_usd,
+                        quote_token_price_usd,
+                        base_token_price_native_currency,
+                        quote_token_price_native_currency,
+                        base_token_price_quote_token,
+                        quote_token_price_base_token,
+                        pool_created_at,
+                        reserve_in_usd,
+                        fdv_usd,
+                        market_cap_usd,
+                        price_change_percentage,
+                        transactions,
+                        volume_usd
+                    },
+                    relationships: {
+                        base_token,
+                        quote_token,
+                        dex
+                    }
+                } = pool;
+
+                return {
+                    id,
+                    type,
+                    name,
+                    address,
+                    base_token_price_usd: roundedTo4Decimals(base_token_price_usd),
+                    quote_token_price_usd: roundedTo4Decimals(quote_token_price_usd),
+                    base_token_price_native_currency: roundedTo4Decimals(base_token_price_native_currency),
+                    quote_token_price_native_currency: roundedTo4Decimals(quote_token_price_native_currency),
+                    base_token_price_quote_token: roundedTo4Decimals(base_token_price_quote_token),
+                    quote_token_price_base_token: roundedTo4Decimals(quote_token_price_base_token),
+                    pool_created_at,
+                    reserve_in_usd: roundedTo4Decimals(reserve_in_usd),
+                    fdv_usd,
+                    market_cap_usd,
+                    price_change_percentage_m5: price_change_percentage ? price_change_percentage.m5 : null,
+                    price_change_percentage_h1: price_change_percentage ? price_change_percentage.h1 : null,
+                    price_change_percentage_h6: price_change_percentage ? price_change_percentage.h6 : null,
+                    price_change_percentage_h24: price_change_percentage ? price_change_percentage.h24 : null,
+                    transactions_m5_buys: transactions ? transactions.m5.buys : null,
+                    transactions_m5_sells: transactions ? transactions.m5.sells : null,
+                    transactions_m5_buyers: transactions ? transactions.m5.buyers : null,
+                    transactions_m5_sellers: transactions ? transactions.m5.sellers : null,
+                    transactions_m15_buys: transactions ? transactions.m15.buys : null,
+                    transactions_m15_sells: transactions ? transactions.m15.sells : null,
+                    transactions_m15_buyers: transactions ? transactions.m15.buyers : null,
+                    transactions_m15_sellers: transactions ? transactions.m15.sellers : null,
+                    transactions_m30_buys: transactions ? transactions.m30.buys : null,
+                    transactions_m30_sells: transactions ? transactions.m30.sells : null,
+                    transactions_m30_buyers: transactions ? transactions.m30.buyers : null,
+                    transactions_m30_sellers: transactions ? transactions.m30.sellers : null,
+                    transactions_h1_buys: transactions ? transactions.h1.buys : null,
+                    transactions_h1_sells: transactions ? transactions.h1.sells : null,
+                    transactions_h1_buyers: transactions ? transactions.h1.buyers : null,
+                    transactions_h1_sellers: transactions ? transactions.h1.sellers : null,
+                    transactions_h24_buys: transactions ? transactions.h24.buys : null,
+                    transactions_h24_sells: transactions ? transactions.h24.sells : null,
+                    transactions_h24_buyers: transactions ? transactions.h24.buyers : null,
+                    transactions_h24_sellers: transactions ? transactions.h24.sellers : null,
+                    volume_usd_m5: volume_usd ? volume_usd.m5 : null,
+                    volume_usd_h1: volume_usd ? volume_usd.h1 : null,
+                    volume_usd_h6: volume_usd ? volume_usd.h6 : null,
+                    volume_usd_h24: volume_usd ? volume_usd.h24 : null,
+                    base_token_id: base_token ? base_token.data.id : null,
+                    quote_token_id: quote_token ? quote_token.data.id : null,
+                    dex_id: dex ? dex.data.id : null
+                };
+            });
+
+            return res.status(200).json(flattenedData);
+        } else {
+            return res.status(response.status).json({ message: 'Multi Pool CoinGecko Data pull failed' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
 
 
 
