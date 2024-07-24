d3d12dfa75eeab323e3e99590d9a003106ed64da
RikeshThapa <rthapa@u.rochester.edu>
2024-07-23 21:22:01 -0700

    sushi is now complete

--- src/app/sushi/easySushi.js
+++ src/app/sushi/easySushi.js
@@ -18,6 +18,11 @@ import { PiCircleFill } from 'react-icons/pi'
 
 import BackgroundVideo from 'next-video/background-video';
 import ethVideo from '/videos/ethVideo.mp4';
+import sushiVid from '/videos/sushi.mp4';
+import sushiVid0 from '/videos/sushi0.mp4';
+import sushiVid1 from '/videos/sushi1.mp4';
+import sushiVid2 from '/videos/sushi2.mp4';
+import sushiVidAI from '/videos/sushiAI.mp4';
 
 export const Highlight = ({
     children,
@@ -214,7 +219,7 @@ const EasySushi = () => {
             <header className='bg-black/10 px-2 py-1 sm:p-6 sm:h-20 flex w-full fixed top-0 gap-4 sm:gap-10 place-items-center'>
                   <div className='flex w-full sm:w-56 text-xs place-items-center gap-2'><Image src="/sushi.png" width="60" height="60" ></Image><Link href={"www.lych3e.com"}>Lychee</Link> / Easy Sushi</div>
                   <div className='text-right text-xs ml-auto'><Link href="#testimonials">Testimonials</Link></div>
-                  <div className='text-right text-xs'><Link href="#demo">Demo</Link></div>
+                  <div className='text-right text-xs'><Link href="#tellMeMore">Learn More</Link></div>
                   <Link href="#getIt"><div className='text-center text-xs bg-[#01A823] px-2 py-3 rounded-md w-32 lg:w-28 cursor-pointer'>Get It</div></Link>
               </header>
               <main className=''>
@@ -234,7 +239,7 @@ const EasySushi = () => {
                       <p className="font-black font-sans text-white text-md py-8">
                         Not anymore
                       </p>
-                      <div className="md:px-28">
+                      <div className="lg:px-28">
                         <div className="font-mono text-white text-md pt-2">
                           <div className="inline-block px-2">EasySushi Lets You Instantly</div>
                           access:
@@ -286,6 +291,12 @@ const EasySushi = () => {
                       </p>
                     </div>
                     <div>
+                      <h1 className="text-white scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">GeckoTerminal</h1>
+                      <p className="font-serif text-sky-200 text-lg mt-4">
+                        GeckoTerminal is a DeFi and DEX aggregator. Explore the market data & prices of any tokens traded across 110+ blockchain networks across 900+ DEXes – brought to you by the same team behind CoinGecko.
+                      </p>
+                    </div>
+                    <div>
                       <h1 className="text-white scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Engage your audience</h1>
                       <p className="font-serif text-sky-200 text-lg mt-4">
                         Professional-quality data graphics and animated stories that bring your data to life.
@@ -332,29 +343,41 @@ const EasySushi = () => {
                   <div className='md:px-20 lg:px-96 w-full'>
                       <h1 className="px-4 md:px-0 text-white scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">3 Simple Steps</h1>
                       <p className="px-4 md:px-0 font-serif text-sky-200 text-lg mt-4">
-                        Step 1: Easily import your data. However you collect your data, Easy Charts can chart it. Build charts from spreadsheet, databases, type it directly or connect to a live data source, like Twitter, Instagram, Youtube, CoinGecko, Yahoo Finance, etc.
+                        Step 0: Pick CoinGecko or GeckoTerminal.
                       </p>
-                      <div className="md:px-10 ">
-                          <Image src={'/uploadMockup.png'} width={800} height={700}/>
+                      <div className="p-4">
+                        <BackgroundVideo src={sushiVid} />
                       </div>
                       <p className="px-4 md:px-0 font-serif text-sky-200 text-lg mt-8">
-                        Step2: Customize and controle absolutely every aspect of your chart.
+                        Step 1: Select an option to explore
                       </p>
-                      <div className="md:px-10 py-10">
-                          <Image src={'/chartGenerated.png'} width={800} height={800}/>
+                      <div  className="p-4">
+                        <BackgroundVideo src={sushiVid0} />
                       </div>
-                      <p className="px-4 md:px-0 font-serif text-sky-200 text-lg mt-4">
-                        Step 3: Share your insights however you like.
+                      <p className="px-4 md:px-0 font-serif text-sky-200 text-lg mt-8">
+                        Step 2: Input your search queries and filters
                       </p>
-                      <div className="md:px-10 py-10">
-                          <Image src={'/customize.png'} width={800} height={800}/>
+                      <div  className="p-4">
+                        <BackgroundVideo src={sushiVid1}/>
+                      </div>
+                      <p className="px-4 md:px-0 font-serif text-sky-200 text-lg mt-8">
+                        Step 3: Unlimited charts and visualizations to gain the deepest insights
+                      </p>
+                      <div  className="p-4">
+                        <BackgroundVideo src={sushiVid2} />
+                      </div>
+                      <p className="px-4 md:px-0 font-serif text-sky-200 text-lg mt-8">
+                        BONUS Step: Go even further and have AI generate a full report your you which you can edit and publish
+                      </p>
+                      <div  className="p-4">
+                        <BackgroundVideo src={sushiVidAI} />
                       </div>
                   </div>                  
                   <div className='px-20 py-36 lg:px-32 w-full' id="testimonials">
                       <h1 className="text-white scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">What Our Legendary Users Have To Say:</h1>
-                      <p className="font-serif text-sky-200 text-lg mt-4 pb-10">People really like is - <br/> All reviews are clickable.                        
+                      <p className="font-serif text-sky-200 text-lg mt-4 pb-10">People really like us - <br/> All reviews are clickable.                        
                       </p>
-                      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden bg-[#0064E6] py-20">
+                      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden bg-black py-20">
                           <Marquee pauseOnHover className="[--duration:20s]">
                               {firstRow.map((review) => (
                               <ReviewCard key={review.username} {...review} />
@@ -365,34 +388,37 @@ const EasySushi = () => {
                               <ReviewCard key={review.username} {...review} />
                               ))}
                           </Marquee>
-                          <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-[#0064E6] from-[#0064E6]"></div>
-                          <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-[#0064E6] from-[#0064E6]"></div>
+                          <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-black"></div>
+                          <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-black"></div>
                       </div>
                   </div>
                   <div className='py-56 px-2 lg:px-10 xl:px-42 w-full text-center grid sm:grid-cols-3 gap-2 place-content-center' id="getIt">
                     <div className='border border-1 border-white rounded-lg py-10 px-6'>
                       <h1 className="text-white scroll-m-20 text-xl font-extrabold tracking-tight lg:text-5xl">Trial</h1>
                       <p className="font-serif text-sky-200 text-lg mt-4">48 hrs free</p>
-                      <p className="font-serif text-sky-200 text-sm">then $6.99/month</p>
+                      <p className="font-serif text-sky-200 text-sm">then $11.99/month</p>
                       <div className='text-left py-4'>
-                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Instant Graphs</p></div>
-                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Wide Variety of Graphs</p></div>
+                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">10 years+ of historical data</p></div>
+                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">110+ blockchain networks</p></div>
+                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">900+ DEXes</p></div>
+                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Unlimited, instant, whide variety of Charts</p></div>
                         <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Simplified User Interface</p></div>
                         <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Unlimited Exports</p></div>
+                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">AI Insights</p></div>
                       </div>                      
-                      <Link href="https://buy.stripe.com/4gw5mE4Gie1g5Ak14v"> <div className='mx-auto my-4 w-32 text-[#0064E6] bg-white hover:bg-white/80 py-2 rounded-lg text-center font-bold'>Try</div> </Link>
+                      <Link href="https://buy.stripe.com/cN2eXegp01eu4wgbJe"> <div className='mx-auto my-4 w-32 text-[#0064E6] bg-white hover:bg-white/80 py-2 rounded-lg text-center font-bold'>Try</div> </Link>
                     </div>
                     <div className='border border-1 border-white rounded-lg py-10 px-6'>
-                      <h1 className="text-white scroll-m-20 text-xl font-extrabold tracking-tight lg:text-5xl">Easy Charts LifeTime</h1>
-                      <p className="font-serif text-sky-200 text-lg mt-4">$13.99/once</p>
+                      <h1 className="text-white scroll-m-20 text-xl font-extrabold tracking-tight lg:text-5xl">EasySushi LifeTime</h1>
+                      <p className="font-serif text-sky-200 text-lg mt-4">$19.99/once</p>
                       <p className="font-serif text-sky-200 text-sm">Limited Time Promo <span className='line-through'>69.99</span></p>
                       <div className='text-left py-4'>
                         <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm"> Includes everything in Trial</p></div>
                         <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">No paying for monthly subscriptions.</p></div>
                         <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">All Future Updates</p></div>
-                        <div className='flex gap-2 place-items-center'><PiCircleFill className='animate-ping text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">11 seats left at this price</p></div>
+                        <div className='flex gap-2 place-items-center'><PiCircleFill className='animate-ping text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">7 seats left at this price</p></div>
                       </div>                      
-                      <Link href="https://buy.stripe.com/3cscP62ya7CS1k414w"> <div className='mx-auto my-4 w-32 text-[#0064E6] bg-white hover:bg-white/80 py-2 rounded-lg text-center font-bold'>Checkout</div> </Link>
+                      <Link href="https://buy.stripe.com/14k3ewgp04qG8MwbJd"> <div className='mx-auto my-4 w-32 text-[#0064E6] bg-white hover:bg-white/80 py-2 rounded-lg text-center font-bold'>Checkout</div> </Link>
                     </div>
                     <div className='border border-1 border-white rounded-lg py-10 px-6'>
                       <Badge className='bg-purple-400'>Most Popular</Badge>
@@ -400,13 +426,13 @@ const EasySushi = () => {
                       <p className="font-serif text-sky-200 text-lg mt-4">$69.99/once</p>
                       <p className="font-serif text-sky-200 text-sm">Limited Time Promo <span className='line-through pl'>$199.99</span></p>
                       <div className='text-left py-4'>
-                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm"> Includes everything in Easy Charts LifeTime</p></div>
+                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm"> Includes everything in EasySushi LifeTime</p></div>
                         <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Includes all current and future Lychee features.</p></div>
                         <Link href="#learnLychee"><p className="underline text-sky-200 text-sm py-4">Click to Learn about lychee</p></Link>
-                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Lychee Scrape any website</p></div>
+                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Scrape any website</p></div>
                         <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Connect to Twitter, Instagran, Yelp, etc</p></div>
-                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">Easy Lychee: convert spreadsheet to website in 0.46 secs</p></div>
-                        <div className='flex gap-2 place-items-center'><PiCircleFill className='animate-ping text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">77 seats left at this price</p></div>
+                        <div className='flex gap-2 place-items-center'><CheckCircle className='text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">EasyLychee: convert spreadsheet to website in 0.46 secs</p></div>
+                        <div className='flex gap-2 place-items-center'><PiCircleFill className='animate-ping text-green-400 h-3 w-3'/><p className="text-sky-200 text-sm">36 seats left at this price</p></div>
                       </div>                      
                       <Link href="https://buy.stripe.com/6oE16o3Ce4qG2o828B"> <div className='mx-auto my-4 w-32 text-[#0064E6] bg-white hover:bg-white/80 py-2 rounded-lg text-center font-bold'>Checkout</div> </Link>
                     </div>
@@ -414,9 +440,9 @@ const EasySushi = () => {
                   <div className='py-10 px-4 md:px-20 lg:px-64 2xl:px-96' id="learnLychee">
                       <h1 className="text-white scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Let's talk about Lychee</h1>
                       <p className="font-serif text-sky-200 text-lg mt-4 pb-10">
-                          Lychee is the Easy Chart's parent (also created by @misterrpink). <br/>
+                          Lychee is team behind EasySushi (also created by @misterrpink). <br/>
                           On a mission to make the whole data pipeline as seamless as possible. <br />
-                          When you get lifetime access to Easy Charts, you also get access to the full suite of tools that is Lychee:
+                          When you get lifetime access to Lychee, you also get access to the full suite of tools that is Lychee:
                       </p>
                       <LycheeFeatureGrid/>                     
                   </div>
