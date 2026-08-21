4fc2d1af6be257f521e779b78c4844c5c184d2b1
RikeshThapa <rthapa@u.rochester.edu>
2026-08-21 22:13:02 +0545

    adding deployment fix 2

---
+++ src/components/ui/checkbox.d.ts
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+
+export declare const Checkbox: React.ForwardRefExoticComponent<
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> &
+    React.RefAttributes<React.ElementRef<typeof CheckboxPrimitive.Root>>
+>;
